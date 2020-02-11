7fd667838b1f1e94fedc0a83e2fa62cdc8726955
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-11 05:26:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,10 @@
 # code here!
-class School
-  attr_accessor :name, :roster
-  
-  def initialize(name)
-    @name = name
-    @roster = {}
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
   end 
+  nu_hash
+end 
   
-  def add_student(student, level)
-    roster[level] ||= []
-    roster[level] << student
-  end 
+end 
