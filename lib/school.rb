cd815a7f09b64319820ac8db3b72c9778e47af87
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-11 05:26:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,15 @@
 # code here!
-def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
-  end 
-  nu_hash
-end 
+class School
+  attr_accessor :name, :roster
   
-end 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
+    roster[level] << student
+  end 
