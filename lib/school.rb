d2cb32bf1cb6a98153af0112048fa4627f5547a1
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-11 05:25:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,10 @@
 # code here!
-class School
-	attr_accessor :roster, :name, :grade 
-	attr_reader :school 
-	
-	def initialize(school)
-    @school = school
-  end
-  
-  def roster
-  	roster = {}
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
   end 
+  nu_hash
+end 
   
-  def add_student(name,grade)
-  	roster[grade] = []
-  	roster[grade] << name
-  		
-  end
-
-end
-
-
-School.new("R").roster.add_student("john",9)
+end 
